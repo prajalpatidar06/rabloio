ee4d896d177266a1d6f01947c910abacd46c0ac9
Prajal Patidar <prajalpatidar01@gmail.com>
2022-10-05 13:54:09 +0530

    commit

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,23 +1,26 @@
 const router = require("express").Router();
 const Book = require("../Books");
 
-router.post("/books", async (req,res)=>{
-    const newBook = new Book({
-        name: req.body.name,
-        ISBN: req.body.isbn,
-        author: req.body.author,
-        price: req.body.price,
-        year: req.body.year,
-        stock: req.body.stock,
-        digitalform: req.body.digitalform,
-    });
+router.get("/books",async(req,res)=>{
+    res.json("yes");
+})
 
-    try {
-        const book = await newBook.save();
-        res.status(201).json(book);
-    } catch (err){
-        res.status(500).json(err);
-    }
+router.post("/books", async (req, res) => {
+  try {
+    const newBook = new Book({
+      name: req.body.name,
+      ISBN: req.body.ISBN,
+      author: req.body.author,
+      price: req.body.price,
+      year: req.body.year,
+      stocks: req.body.stocks,
+      digitalform: req.body.digitalform,
+    });
+    const book = await newBook.save();
+    res.status(201).json(book);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
